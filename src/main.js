8763979fb79261b00b803c5edf3f1cc1dca134db
Vitya Poprozhuk <poprozhuk.vitya2@gmail.com>
2022-09-16 18:57:31 +0300

    add intersection observer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { vIntersection } from './directives'
 
 Vue.config.productionTip = false
+
+Vue.directive('intersection', vIntersection);
 
 new Vue({
   render: h => h(App),
