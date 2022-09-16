e046cb2af4bde61980752453513875f8c98a5b80
Vitya Poprozhuk <poprozhuk.vitya2@gmail.com>
2022-09-16 19:57:39 +0300

    add draft version of functionality

--- src/components/Autocomplete/Autocomplete.vue
+++ src/components/Autocomplete/Autocomplete.vue
@@ -4,10 +4,23 @@
             Label
         </label>
         <div class="input-container">
-            <input type="text" placeholder="Search" id="autocomplete" v-model="searchQuery">
-            <button class="clear-input" type="button">X</button>
-            <ul class="options-list">
-                <li class="option-item" v-for="option in filteredOptions" :key="option.id">{{option.title}}</li>
+            <input
+                class="query-input"
+                type="text"
+                placeholder="Search"
+                id="autocomplete"
+                v-model="searchQuery"
+                @click="isListVisible = true"
+                @blur="onBlur"
+                @keydown="onKeyDown"
+                autocomplete="off"
+            >
+            <button class="clear-input" type="button" aria-label="Clear input" @click="clearInput" v-if="searchQuery">
+                <img src="../../icons/icon-close.svg" />
+            </button>
+            <ul class="options-list" v-if="isListVisible" ref="optionsList">
+                <li class="option-item" @mouseenter="highlightedOption = index" :class="{active: highlightedOption === index, selected: option === selectedOption}" v-for="(option, index) in filteredOptions" :key="option.id" @mousedown="selectOption(option)">{{option.title}}</li>
+<!--                <li class="observer" v-intersection="showMore"></li>-->
             </ul>
         </div>
     </div>
@@ -22,6 +35,8 @@
         searchQuery: '',
         showOptions: 15,
         selectedOption: null,
+        isListVisible: false,
+        highlightedOption: null
       }
     },
     computed: {
@@ -34,7 +49,53 @@
         }
         return options.filter(option => option.title.toLowerCase().includes(this.searchQueryLowerCase)).slice(0, this.showOptions)
       }
-    }
+    },
+    watch: {
+      searchQuery(newValue) {
+        if (!newValue) {
+          this.selectedOption = null;
+        }
+      }
+    },
+    methods: {
+      showMore() {
+        this.showOptions += 15;
+      },
+      selectOption(option) {
+        this.selectedOption = option;
+        this.searchQuery = option.title;
+        this.isListVisible = false;
+      },
+      onBlur() {
+        this.isListVisible = false;
+        this.searchQuery = this.selectedOption?.title  || "";
+        this.highlightedOption = null;
+      },
+      clearInput() {
+        this.searchQuery = "";
+        this.selectedOption = null;
+      },
+      onKeyDown({ key }) {
+        if (key === 'ArrowUp') {
+          if (this.highlightedOption !== null) {
+            this.highlightedOption = this.highlightedOption === 0 ? this.filteredOptions.length - 1 : this.highlightedOption - 1;
+          } else {
+            this.highlightedOption = this.filteredOptions.length - 1;
+          }
+        } else if (key === 'ArrowDown') {
+          if (this.highlightedOption !== null) {
+            this.highlightedOption = this.filteredOptions.length - 1 === this.highlightedOption ? 0 : this.highlightedOption + 1;
+          } else {
+            this.highlightedOption = 0;
+          }
+        } else if (key === 'Enter') {
+          if (this.highlightedOption) {
+            this.selectOption(this.filteredOptions[this.highlightedOption]);
+          }
+        }
+        this.$refs.optionsList.scrollTop = this.highlightedOption * 30;
+      }
+    },
   }
 </script>
 
