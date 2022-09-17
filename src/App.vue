0250560485b266d4f0f5cdbe205e5432af87a4e4
Vitya Poprozhuk <poprozhuk.vitya2@gmail.com>
2022-09-17 09:40:59 +0300

    add styles like in material ui

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,26 @@
 </template>
 
 <script>
-  import Autocomplete from './components/Autocomplete/Autocomplete.vue'
-  export default {
+import Autocomplete from './components/Autocomplete/Autocomplete.vue'
 
-    name: 'App',
-    components: {
-      Autocomplete
-    }
+export default {
+  name: 'App',
+  components: {
+    Autocomplete
   }
+}
 </script>
+
+<style>
+body {
+  font-size: 16px;
+  font-family: Arial, sans-serif;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
+}
+</style>
 
